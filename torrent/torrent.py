8ef7e081299575994ebb6fe55603f15b48b16eee
moowlf <tiago.lpneto@outlook.com>
2024-06-12 12:28:21 +0100

    ..

--- torrent/torrent.py
+++ torrent/torrent.py
@@ -1,5 +1,4 @@
 import dataclasses
-import math
 import threading
 import time
 import socket
@@ -41,6 +40,10 @@ class Torrent:
 
         # Prepare all pieces to be downloaded for this torrent
         self.pieces_to_download = self._divide_into_blocks()
+        self._to_complete_pieces = self.pieces_to_download.qsize()
+
+    def is_complete(self):
+        return self._to_complete_pieces == 0
 
     def download(self, own_peer_id: str, threads=1):
         # Start thread responsible for communicating with tracker
@@ -48,17 +51,32 @@ class Torrent:
         tracker_comm.start()
 
         # Start threads responsible for downloading the pieces
-        workers = [threading.Thread(target=self._download_from_peer, args=(own_peer_id,)) for _ in range(threads)]
-        [worker.start() for worker in workers]
+        self._download(own_peer_id)
 
         # End threads
         tracker_comm.join()
-        [worker.join() for worker in workers]
 
         with open(self._metadata.file_name, "wb") as file:
             self.data.sort(key=lambda x: x[0])
             for piece in self.data:
                 file.write(piece[1])
+
+    def _download(self, own_peer_id):
+
+        threads = []
+        while not self.is_complete():
+
+            try:
+                peer = self._peers.get(timeout=5)
+                work = self.pieces_to_download.get(timeout=5)
+            except Exception:
+                continue
+
+            threads.append(
+                threading.Thread(target=self._download_from_peer, args=(own_peer_id, peer, work), name=peer["ip"]))
+            threads[-1].start()
+
+        [thread.join() for thread in threads]
 
     def _get_peers(self, own_peer_id: str):
 
@@ -70,51 +88,52 @@ class Torrent:
             # Decode the answer and wait for next call
             answer = bencode.decode_dictionary(req.content)[0]
 
+            while not self._peers.empty():
+                self._peers.get()
+
             for peer in answer["peers"]:
                 self._peers.put({"ip": peer["ip"].decode(), "port": peer["port"]})
 
             # Sleep
-            # time.sleep(answer["interval"])
-            time.sleep(5)
+            print(f"Peer request: Waiting for {answer['interval']}s")
+            time.sleep(answer["interval"])
 
-    def _download_from_peer(self, own_peer_id: str):
+    def _download_from_peer(self, own_peer_id: str, peer: dict, piece_to_download: Piece):
 
-        while not self.pieces_to_download.empty():
-            # Retrieve piece to be downloaded
-            piece_to_download: Piece = self.pieces_to_download.get()
+        # Retrieve IP to connect (assuming every IP has all files)
+        peer_ip, peer_port = peer["ip"], peer["port"]
 
-            # Retrieve IP to connect (assuming every IP has all files)
-            peer = self._peers.get()
-            peer_ip, peer_port = peer["ip"], peer["port"]
+        # Start the connection with chosen peer
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((peer_ip, peer_port))
+        print(f"> Connected to {peer_ip}:{peer_port}")
 
-            # Start the connection with chosen peer
-            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            conn.connect((peer_ip, peer_port))
+        # Send Handshake and receive
+        handshake = connection.build_handshake(self._metadata_infoHash, own_peer_id)
+        conn.send(handshake)
+        _ = self._receive_data(conn, 256)
 
-            # Send Handshake and receive
-            handshake = connection.build_handshake(self._metadata_infoHash, own_peer_id)
-            conn.send(handshake)
-            _ = self._receive_data(conn, 256)
+        # Send interested
+        interested = connection.build_interested()
+        conn.send(interested)
 
-            # Send interested
-            interested = connection.build_interested()
-            conn.send(interested)
+        # From here onwards, we can receive some messages "out of order".
+        current_state = "chocked"
 
-            # From here onwards, we can receive some messages "out of order".
-            current_state = "chocked"
+        for block_piece in piece_to_download.blocks:
+            block_piece.data, current_state = self._download_state_machine(conn, block_piece, current_state)
 
-            for block_piece in piece_to_download.blocks:
-                block_piece.data, current_state = self._download_state_machine(conn, block_piece, current_state)
+        data = piece_to_download.blocks[0].data + piece_to_download.blocks[1].data
+        res = sha1(data)
 
-            data = piece_to_download.blocks[0].data + piece_to_download.blocks[1].data
-            res = sha1(data)
+        print(res.hexdigest(), piece_to_download.hash.hex())
+        if piece_to_download.hash.hex() != res.hexdigest():
+            self.pieces_to_download.put(piece_to_download)
+            return
 
-            if piece_to_download.hash.hex() != res.hexdigest():
-                self.pieces_to_download.put(piece_to_download)
-
-            self.data.append([piece_to_download.piece_id, data])
-
-        return
+        print("> Hashes matched")
+        self.data.append([piece_to_download.piece_id, data])
+        self._to_complete_pieces -= 1
 
     def _calculate_encoded_info_hash(self):
         info = {
@@ -171,7 +190,6 @@ class Torrent:
 
             if len(downloaded_buffer) < size_buffer:
                 break
-
         return answer
 
     @staticmethod
@@ -193,7 +211,7 @@ class Torrent:
             if not already_request_piece and current_state == "unchocked":
                 already_request_piece = True
                 conn.send(data)
-            time.sleep(2)
+                time.sleep(2)
 
             answer = Torrent._receive_data(conn, block_length)
             _, bitfield, payload = connection.parse_peer_message(answer)
@@ -205,31 +223,16 @@ class Torrent:
 
             # Chocked message
             if bitfield == 0:
-                print("received chocked message")
                 current_state = "chocked"
             elif bitfield == 1:
-                print("received unchocked message")
                 current_state = "unchocked"
-            elif bitfield == 2:
-                print("received interested message")
-            elif bitfield == 3:
-                print("received not interested message")
-            elif bitfield == 4:
-                print("received have message")
             elif bitfield == 5:
-                print("received bitfield message")
                 # Send that we're interested in this piece
                 conn.send(connection.build_interested())
                 continue
-            elif bitfield == 6:
-                print("received request message")
-
             elif bitfield == 7:
-                print("received pieces message")
                 i, b, bl = connection.parse_piece(payload)
                 return bl, current_state
 
-            elif bitfield == 8:
-                print("received cancel message")
 
         return b"", current_state
