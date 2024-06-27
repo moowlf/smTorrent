a041d0d6dc028322aa507ba181f12ec89c96e6fa
moowlf <tiago.lpneto@outlook.com>
2024-06-27 15:21:58 +0100

    Output improvements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
+import os
 import sys
+import time
+import signal
 import base64
 import logging
 
@@ -10,7 +12,12 @@ from torrent.Session import Session
 from torrent.Torrent import Torrent
 
 # Configure logging
-logging.basicConfig(filename="smtorrent.log", level=logging.INFO, format='[%(asctime)s][%(levelname)s] - %(message)s')
+LOG_FILE = "smtorrent.log"
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="[%(asctime)s][%(levelname)s] - %(message)s",
+)
 
 # Torrent file
 if len(sys.argv) != 2:
@@ -22,12 +29,70 @@ current_session = Session()
 
 logging.log(logging.INFO, f"Opening file {filepath}")
 
-with open(filepath, 'rb') as f:
+# Register the signal handler
+signal.signal(signal.SIGINT, lambda signum, frame: current_session.terminate())
+
+
+# Read n lines from the end of file
+def tail_file(file, n):
+
+    with open(file, "rb") as f:
+        try:
+            f.seek(-2, os.SEEK_END)
+            
+            while n > 0:
+
+                if f.read(1) == b"\n":
+                    n -= 1
+    
+                f.seek(-2, os.SEEK_CUR)
+
+        except OSError:
+            f.seek(0)
+
+        return [line.decode() for line in f.readlines()]
+
+
+
+# Output the torrent information
+def output_torrent_information_to_console():
+
+    os.system("clear" if os.name == "posix" else "cls")
+
+    # ==== Torrents =====
+    cols = (120 - len(" Torrents ")) // 2
+    print("*" * cols + " Torrents " + "*" * cols)
+    print()
+
+    # print torrents
+    for torrent in current_session.torrents():
+        print(torrent)
+    print()
+
+    # ==== Logs =====
+    cols = (120 - len(" Logs ")) // 2
+    print("*" * cols + " Logs " + "*" * cols)
+    print()
+
+    log_lines = tail_file(LOG_FILE, 10)
+    for line in log_lines:
+        print(line.rstrip() if len(line) < 120 else line[:117] + "...")
+
+
+with open(filepath, "rb") as f:
     d = bencode.decode_dictionary(f.read())[0]
 
     torrent_information = TorrentInformation(d)
-    logging.log(logging.INFO, f"Torrent information: {base64.encodebytes(str(torrent_information).encode())}")
+    logging.log(
+        logging.INFO,
+        f"Torrent information: {base64.encodebytes(str(torrent_information).encode())}",
+    )
 
     current_session.add_torrent(Torrent(torrent_information))
     current_session.download()
+
+    while not current_session.is_terminated():
+        output_torrent_information_to_console()
+        time.sleep(2)
+    
     current_session.wait_to_close()
--- torrent/Session.py
+++ torrent/Session.py
@@ -11,14 +11,9 @@ class Session:
 
         # Register the signal handler
         self._should_terminate = False
-        signal.signal(signal.SIGINT, lambda signum, frame: self._terminate())
 
         # List of torrents currently being downloaded / seeded
         self._torrent_files = []
-
-        # Thread responsible for logging
-        self._logging_thread = threading.Thread(target=self._status)
-        self._logging_thread.start()
 
         # Threads responsible for downloading the pieces
         self._downloading_threads = []
@@ -37,30 +32,19 @@ class Session:
             self._downloading_threads.append(threading.Thread(target=torrent.download, args=(self._peerID,)))
             self._downloading_threads[-1].start()
     
-    def _status(self):
-
-        from time import sleep
-        import os
-
-        while not self._should_terminate:
-
-            os.system("clear" if os.name == "posix" else "cls")
-            for torrent in self._torrent_files:
-                print(torrent)
-            
-            # Sleep for 5 seconds
-            sleep(5)
-    
     def wait_to_close(self):
-        self._logging_thread.join()
         for thread in self._downloading_threads:
             thread.join()
 
     def torrents(self):
-        return self._torrents
+        return self._torrent_files
     
-    def _terminate(self):
+    def terminate(self):
         self._should_terminate = True
         logging.log(logging.INFO, "Terminating session...")
         for torrent in self._torrent_files:
             torrent.terminate()
+    
+    def is_terminated(self):
+        return self._should_terminate
+
